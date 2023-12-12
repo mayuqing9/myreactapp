57f288b9d06b7860ee719336f5697a0df0dd64d6
simplyekom <simplyekom@gmail.com>
2023-12-11 19:06:13 -0700

    inserting recipes in array - in progess

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+
 const App = () => {
   const APP_ID = "8261458d";
   const APP_KEY = "25eb968a4ea3c128f462fa861ff1786f";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+
+useEffect ( () => {
+  getRecipes();
+
+
+}, []);
+
+const getRecipes = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  setRecipes(data.hits);
+}
 
   return (
     <div className="App">
