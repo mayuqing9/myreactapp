8c5c88e34f721223db4e65db220d6df85ce319d0
simplyekom <simplyekom@gmail.com>
2023-12-11 19:29:32 -0700

    now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -17,6 +18,7 @@ const getRecipes = async () => {
   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
   setRecipes(data.hits);
+  console.log(data.hits);
 }
 
   return (
@@ -25,6 +27,13 @@ const getRecipes = async () => {
         <input className="search-bar" type= "text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipes => (
+        <Recipe key={recipes.recipe.label}
+        title={recipes.recipe.label}
+        calories={recipes.recipe.calories}
+        image={recipes.recipe.image}  />
+
+      ))}
     </div>
   );
 }
