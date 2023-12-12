a2461021457563cc67765a6df94e215583584156
simplyekom <simplyekom@gmail.com>
2023-12-11 18:48:09 -0700

    search-button completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+const App = () => {
+  const APP_ID = "8261458d";
+  const APP_KEY = "25eb968a4ea3c128f462fa861ff1786f";
 
-function App() {
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type= "text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
